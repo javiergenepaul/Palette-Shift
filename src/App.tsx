9694008aa4106997ce6ca7bcb411d9458c756112
javiergenepaul <javiergenepaul@gmail.com>
2024-03-20 15:00:41 +0800

    install toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "./components";
 import { ThemeProvider } from "./provider";
 import { Main } from "./screens/main";
 
 function App() {
   return (
     <ThemeProvider>
+      <Toaster />
       <Main />
     </ThemeProvider>
   );
